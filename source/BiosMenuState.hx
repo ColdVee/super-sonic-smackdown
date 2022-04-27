c52079e91bd3e1b57aef29b16e7b17e2b121bbd9
Cold_Vee <baselfares283@gmail.com>
2022-04-27 17:14:44 +0300

    cleaned up bios it really needed it

--- source/BiosMenuState.hx
+++ source/BiosMenuState.hx
@@ -28,9 +28,6 @@ class BiosMenuState extends MusicBeatState
 {
 	public static var psychEngineVersion:String = '0.5.1-git'; //This is also used for Discord RPC
 	public static var curSelected:Int = 0;
-
-	var charSpr:FlxSprite;
-	var portrait:FlxSprite;
 	var bioText:FlxText;
 	private var camGame:FlxCamera;
 	private var camAchievement:FlxCamera;
@@ -41,6 +38,9 @@ class BiosMenuState extends MusicBeatState
 		'terios',
 		'sonai'
 	];
+	var bios:Array<String> = [];
+	var charSprites:Map<String, Dynamic> = [];
+	var portSprites:Map<String, Dynamic> = [];
 	var camFollow:FlxObject;
 	var camFollowPos:FlxObject;
 	var debugKeys:Array<FlxKey>;
@@ -54,21 +54,11 @@ class BiosMenuState extends MusicBeatState
 		DiscordClient.changePresence("In the Bios Menu", null);
 		#end
 
-		camGame = new FlxCamera();
-
-		FlxG.cameras.reset(camGame);
-		FlxCamera.defaultCameras = [camGame];
-
 		transIn = FlxTransitionableState.defaultTransIn;
 		transOut = FlxTransitionableState.defaultTransOut;
 
 		persistentUpdate = persistentDraw = true;
 		
-		camFollow = new FlxObject(0, 0, 1, 1);
-		camFollowPos = new FlxObject(0, 0, 1, 1);
-		add(camFollow);
-		add(camFollowPos);
-
 		var bg:FlxSprite = new FlxSprite(0, 0).loadGraphic(Paths.image('bios/bg'));
 		bg.scrollFactor.set(0, 0);
 		bg.antialiasing = ClientPrefs.globalAntialiasing;
@@ -81,50 +71,6 @@ class BiosMenuState extends MusicBeatState
 		uiShadow.updateHitbox();
 		uiShadow.scrollFactor.set();
 		add(uiShadow);
-		
-		//stupid fucking way to precache al lof this shit so that it doesnt lag when selected i know just WHATEVER RAAGH IS UCK AT CODING FUUCK
-		
-		charSpr = new FlxSprite(867, 122.4);
-		charSpr.frames = Paths.getSparrowAtlas('characters/terios', 'shared');
-		charSpr.animation.addByPrefix('idle', 'Terios IDLE0', 24, false);
-		charSpr.animation.play('idle', true);
-		charSpr.setGraphicSize(283);
-		charSpr.updateHitbox();
-		charSpr.scrollFactor.set();
-		add(charSpr);
-		
-		remove(charSpr);
-		charSpr.kill();
-		charSpr = new FlxSprite(880.85, 209);
-		charSpr.frames = Paths.getSparrowAtlas('characters/Proto_Silver_Sonic', 'shared');
-		charSpr.animation.addByPrefix('idle', 'Proto IDLE', 24, false);
-		charSpr.animation.play('idle', true);
-		charSpr.setGraphicSize(208);
-		charSpr.updateHitbox();
-		charSpr.scrollFactor.set();
-		add(charSpr);
-		
-		remove(charSpr);
-		charSpr.kill();
-		charSpr = new FlxSprite(847.35, 83.45);
-		charSpr.frames = Paths.getSparrowAtlas('characters/DarkSonic_Assets', 'shared');
-		charSpr.animation.addByPrefix('idle', 'Dark Sonic IDLE', 24, false);
-		charSpr.animation.play('idle', true);
-		charSpr.setGraphicSize(278);
-		charSpr.updateHitbox();
-		charSpr.scrollFactor.set();
-		add(charSpr);
-		
-		remove(charSpr);
-		charSpr.kill();
-		charSpr = new FlxSprite(855.6, 107.5);
-		charSpr.frames = Paths.getSparrowAtlas('characters/impo', 'shared');
-		charSpr.animation.addByPrefix('idle', 'SAI IDLE', 24, false);
-		charSpr.animation.play('idle', true);
-		charSpr.setGraphicSize(305);
-		charSpr.updateHitbox();
-		charSpr.scrollFactor.set();
-		add(charSpr);
 		
 		var plate:FlxSprite = new FlxSprite(237.05, 535.05).loadGraphic(Paths.image('bios/plate'));
 		plate.setGraphicSize(812);
@@ -140,43 +86,61 @@ class BiosMenuState extends MusicBeatState
 		chalkboard.antialiasing = ClientPrefs.globalAntialiasing;
 		add(chalkboard);
 		
-		portrait = new FlxSprite(234, 128).loadGraphic(Paths.image('bios/portrait-dark'));
-		portrait.setGraphicSize(296);
-		portrait.updateHitbox();
-		portrait.scrollFactor.set();
-		portrait.antialiasing = ClientPrefs.globalAntialiasing;
-		add(portrait);
+		//stupid fucking way to precache al lof this shit so that it doesnt lag when selected i know just WHATEVER RAAGH IS UCK AT CODING FUUCK
 		
-		remove(portrait);
-		portrait.destroy();
-		portrait = new FlxSprite(234, 128).loadGraphic(Paths.image('bios/portrait-silver'));
-		portrait.setGraphicSize(296);
-		portrait.updateHitbox();
-		portrait.scrollFactor.set();
-		portrait.antialiasing = ClientPrefs.globalAntialiasing;
-		add(portrait);
-		
-		remove(portrait);
-		portrait.destroy();
-		portrait = new FlxSprite(234, 128).loadGraphic(Paths.image('bios/portrait-terios'));
-		portrait.setGraphicSize(296);
-		portrait.updateHitbox();
-		portrait.scrollFactor.set();
-		portrait.antialiasing = ClientPrefs.globalAntialiasing;
-		add(portrait);
-		
-		remove(portrait);
-		portrait.destroy();
-		portrait = new FlxSprite(234, 128).loadGraphic(Paths.image('bios/portrait-sonai'));
-		portrait.setGraphicSize(296);
-		portrait.updateHitbox();
-		portrait.scrollFactor.set();
-		portrait.antialiasing = ClientPrefs.globalAntialiasing;
-		add(portrait);
+		for (i in 0...chars.length)
+		{
+			var charSpr:FlxSprite = new FlxSprite();
+			charSpr.frames = Paths.getSparrowAtlas('bios/menu_chars');
+			charSpr.animation.addByPrefix(chars[i], chars[i], 24, false);
+			charSpr.animation.play(chars[i], true);
+			switch (chars[i])
+			{
+				case 'silver':
+					bios[i] = "Silver Sonic\nVoiced by Luckiibean\nSonic's first robotic doppelganger created by Dr.Eggman.\n\nThis guy has made an appearance in:\nSonic 2 8-BIT, Sonic Mania and both the Archie and IDW comics.";
+					charSpr.setGraphicSize(208);
+					charSpr.setPosition(880.85, 209);
+				case 'dark':
+					bios[i] = "Dark Sonic\nVoiced by Snap\nOne of Sonic's many super forms,\nmanifested from his anger & the energy from counterfeit Chaos Emeralds\nAlthough, he only appeared once in\nSonic X in The Season 3 Episode: Testing Time.";
+					charSpr.setGraphicSize(273);
+					charSpr.setPosition(847.35, 83.45);
+				case 'terios':
+					bios[i] = "Terios\nVoiced by Begwhi\nA character based on one of Shadow's original character designs for:\nSonic Adventure 2\n\nGiven the name Terios (or Umbra) by the community.";
+					charSpr.setGraphicSize(283);
+					charSpr.setPosition(867, 122.4);
+				case 'sonai':
+					bios[i] = "Extrapolator/SonAI\nExtrapolator (or SonAI for short) is Simply EJ's EXE take on the blue blur. \nIn summary, its an AI that was meant to assist in making Sonic 2 that was\nreactivated in a corrupted copy of the game,\nyears after it's released causing it to be corrupted itself.";
+					charSpr.setGraphicSize(305);
+					charSpr.setPosition(855.6, 107.5);
+			}
+			charSpr.updateHitbox();
+			charSpr.scrollFactor.set();
+			charSpr.ID = i;
+			charSprites[chars[i]] = charSpr;
+			add(charSpr);
+			
+			var portrait:FlxSprite = new FlxSprite().loadGraphic(Paths.image('bios/portrait-' + chars[i]));
+			portrait.setGraphicSize(296);
+			switch (chars[i])
+			{
+				case 'silver':
+					portrait.setPosition(234, 132);
+				default:
+					portrait.setPosition(234, 128);
+			}
+			portrait.updateHitbox();
+			portrait.scrollFactor.set();
+			portrait.ID = i;
+			portSprites[chars[i]] = portrait;
+			add(portrait);
+		}
 		
 		bioText = new FlxText(0, 0, 0, "Dark Sonic\rVoiced by Snap\rOne of Sonic's many super forms,\rachieved from his anger and the energy from counterfeit Chaos Emeralds \rAlthough, this only appeared once in Sonic X Season 3 Episode 67 Testing Time.", 12);
 		bioText.setFormat("PhantomMuff 1.5", 16, FlxColor.WHITE, CENTER);
 		bioText.scrollFactor.set();
+		bioText.fieldWidth = -5;
+		bioText.wordWrap = false;
+		bioText.autoSize = false;
 		bioText.setPosition(plate.getMidpoint().x - 325, plate.getMidpoint().y - 60);
 		add(bioText);
 
@@ -184,8 +148,6 @@ class BiosMenuState extends MusicBeatState
 		/*if(optionShit.length > 6) {
 			scale = 6 / optionShit.length;
 		}*/
-
-		FlxG.camera.follow(camFollowPos, null, 1);
 
 		// NG.core.calls.event.logEvent('swag').send();
 
@@ -229,7 +191,6 @@ class BiosMenuState extends MusicBeatState
 		}
 
 		var lerpVal:Float = CoolUtil.boundTo(elapsed * 7.5, 0, 1);
-		camFollowPos.setPosition(FlxMath.lerp(camFollowPos.x, camFollow.x, lerpVal), FlxMath.lerp(camFollowPos.y, camFollow.y, lerpVal));
 
 		if (!selectedSomethin)
 		{
@@ -268,85 +229,38 @@ class BiosMenuState extends MusicBeatState
 	{
 		super.beatHit();
 
-		if (charSpr != null && curBeat % 1 == 0)
-			charSpr.animation.play('idle', true);
+		if (charSprites[chars[curSelected]] != null && curBeat % 1 == 0)
+			charSprites[chars[curSelected]].animation.play(chars[curSelected], true);
 	}
 
 
 	function changeItem(huh:Int = 0)
 	{
 		curSelected += huh;
-		
-		bioText.fieldWidth = -5;
-		bioText.wordWrap = false;
-		bioText.autoSize = false;
 		if (curSelected >= chars.length) //revert back to without - 1
 			curSelected = 0;
 		if (curSelected < 0)
 			curSelected = chars.length - 1; //revert back to - 1
-			
-		remove(portrait);
-		portrait.destroy();
-		portrait = new FlxSprite(234, 128).loadGraphic(Paths.image('bios/portrait-'+chars[curSelected]));
-		portrait.setGraphicSize(296);
-		portrait.updateHitbox();
-		portrait.scrollFactor.set();
-		portrait.antialiasing = ClientPrefs.globalAntialiasing;
-		add(portrait);
-			
-		switch (curSelected) // 0 = silver, 1 = dark, 2 = terios, 3 = sonai
+		
+		for (sprite in charSprites)
 		{
-			case 0:
-				remove(charSpr);
-				charSpr.kill();
-				charSpr = new FlxSprite(880.85, 209);
-				charSpr.frames = Paths.getSparrowAtlas('characters/Proto_Silver_Sonic', 'shared');
-				charSpr.animation.addByPrefix('idle', 'Proto IDLE', 24, false);
-				charSpr.animation.play('idle', true);
-				charSpr.setGraphicSize(208);
-				charSpr.updateHitbox();
-				charSpr.scrollFactor.set();
-				add(charSpr);
-				
-				bioText.text = "Silver Sonic\nVoiced by Luckiibean\nSonic's first robotic doppelganger created by Dr.Eggman.\n\nThis guy has made an appearance in:\nSonic 2 8-BIT, Sonic Mania and both the Archie and IDW comics.";
-			case 1:
-				remove(charSpr);
-				charSpr.kill();
-				charSpr = new FlxSprite(847.35, 83.45);
-				charSpr.frames = Paths.getSparrowAtlas('characters/DarkSonic_Assets', 'shared');
-				charSpr.animation.addByPrefix('idle', 'Dark Sonic IDLE', 24, false);
-				charSpr.animation.play('idle', true);
-				charSpr.setGraphicSize(278);
-				charSpr.updateHitbox();
-				charSpr.scrollFactor.set();
-				add(charSpr);
-				
-				bioText.text = "Dark Sonic\nVoiced by Snap\nOne of Sonic's many super forms,\nmanifested from his anger & the energy from counterfeit Chaos Emeralds\nAlthough, he only appeared once in\nSonic X in The Season 3 Episode: Testing Time.";
-			case 2:
-				remove(charSpr);
-				charSpr.kill();
-				charSpr = new FlxSprite(867, 122.4);
-				charSpr.frames = Paths.getSparrowAtlas('characters/terios', 'shared');
-				charSpr.animation.addByPrefix('idle', 'Terios IDLE0', 24, false);
-				charSpr.animation.play('idle', true);
-				charSpr.setGraphicSize(283);
-				charSpr.updateHitbox();
-				charSpr.scrollFactor.set();
-				add(charSpr);
-				
-				bioText.text = "Terios\nVoiced by Begwhi\nA character based on one of Shadow's original character designs for:\nSonic Adventure 2\n\nGiven the name Terios (or Umbra) by the community.";
-			case 3:
-				remove(charSpr);
-				charSpr.kill();
-				charSpr = new FlxSprite(855.6, 107.5);
-				charSpr.frames = Paths.getSparrowAtlas('characters/impo', 'shared');
-				charSpr.animation.addByPrefix('idle', 'SAI IDLE', 24, false);
-				charSpr.animation.play('idle', true);
-				charSpr.setGraphicSize(305);
-				charSpr.updateHitbox();
-				charSpr.scrollFactor.set();
-				add(charSpr);
-				bioText.text = "Extrapolator/SonAI\nExtrapolator (or SonAI for short) is Simply EJ's EXE take on the blue blur. \nIn summary, its an AI that was meant to assist in making Sonic 2 that was\nreactivated in a corrupted copy of the game,\nyears after it's released causing it to be corrupted itself.";
+			if (sprite.ID == curSelected)
+			{
+				charSprites[chars[curSelected]].animation.play(chars[curSelected], true);
+				sprite.visible = true;
+			}
+			else
+				sprite.visible = false;
 		}
+		
+		for (sprite in portSprites)
+		{
+			if (sprite.ID == curSelected)
+				sprite.visible = true;
+			else
+				sprite.visible = false;
+		}
+		
+		bioText.text = bios[curSelected];
 	}
 }
